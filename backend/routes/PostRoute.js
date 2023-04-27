8853c45d7d94614f619c88313594e3c31825a899
nicklin-sjsu <nicklin1219@gmail.com>
2023-04-27 10:57:57 -0700

    remove unused routes

--- backend/routes/PostRoute.js
+++ backend/routes/PostRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {postTwitter} = require("../controllers/PostController");
+const {postTwitter, getPostHistory} = require("../controllers/PostController");
 
 router.get("/twitter", postTwitter);
+router.get("/twitter", getPostHistory);
 
 module.exports = router;
