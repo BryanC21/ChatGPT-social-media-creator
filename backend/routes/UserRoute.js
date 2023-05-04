c439e2a947da534d52035da9e025cdce11aa5f68
Nohar Gurrala Reddy <gurralachintu@gmail.com>
2023-05-04 16:38:08 -0700

    User Controller

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {getEmployeeCount, getByID, getByDepartment, getDepartments, editByID, getDepartmentsManagers, getByDepartmentCurrent, getTitles} = require("../controllers/UserController");
+const {getByID, editByID} = require("../controllers/UserController");
 
-router.get("/getEmployeeCount", getEmployeeCount);
 router.get("/getByID", getByID);
-router.get("/getByDepartment", getByDepartment);
-router.get("/getDepartments", getDepartments);
-router.get("/getTitles", getTitles);
-router.get("/getDepartmentsManagers", getDepartmentsManagers);
 router.get("/editByID", editByID);
-router.get("/getByDepartmentCurrent", getByDepartmentCurrent);
 
 module.exports = router;
