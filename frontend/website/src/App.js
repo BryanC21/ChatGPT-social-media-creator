a623ac00be6b79fc282ff6da3d29313e4827b7a0
Nohar Gurrala Reddy <gurralachintu@gmail.com>
2023-05-11 13:46:11 -0700

    UserPage

--- frontend/website/src/App.js
+++ frontend/website/src/App.js
@@ -8,6 +8,7 @@ import MyNavbar from "./components/MyNavbar";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
+import UserPage from "./pages/UserPage";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/logout" element={<Logout />} />
+        <Route path="/user" element={<UserPage} />
         <Route element={<Error />} />
       </Routes>
     </div>
--- frontend/website/src/pages/UserPage.js
+++ frontend/website/src/pages/UserPage.js
@@ -2,11 +2,7 @@ import { Container, Row, Col, Image } from 'react-bootstrap';
 import axios from "axios";
 import * as React from 'react';
 import { useNavigate, useParams } from "react-router-dom";
-import { getToken } from '../util';
-import Post from "../../../../backend/database/models/Post";
-import Platform from "../../../../backend/database/models/Platform";
-import User from "../../../../backend/database/models/User";
-import { getByPost } from "../../../../backend/controllers/UserController";
+
 
 
 const UserPage = () => {
@@ -18,11 +14,7 @@ const UserPage = () => {
     const [user, setUser] = React.useState({});
 
     React.useEffect(() => {
-        axios.get(`http://localhost:5003/api/user/getCurrentUser`, {
-            headers: {
-                authorization: `Bearer ${getToken()}`
-            }
-        })
+        axios.get(`http://localhost:5003/api/user/getCurrentUser`, {})
             .then(function (response) {
                 // console.log(response.data)
                 setUser(response.data.results);
@@ -35,11 +27,7 @@ const UserPage = () => {
 
     const [post, getByPost] = React.useState({});
     React.useEffect(() => {
-        axios.get(`http://localhost:5003/api/post/getPostHistory`, {
-            headers: {
-                authorization: `Bearer ${getToken()}`
-            }
-        })
+        axios.get(`http://localhost:5003/api/post/getPostHistory`, {})
             .then(function (response) {
                 // console.log(response.data)
                 getByPost(response.data.results);
@@ -53,25 +41,23 @@ const UserPage = () => {
 
     return (
         <>
-            <Header />
             <Container fluid style={{ marginTop: 36, marginBottom: 36 }}>
                 <Row>
                     <Col md={6} className="d-flex align-items-center justify-content-center">
-                        <Image src={'../../User.png'} />
+                        <Image src={'../../User.png'} fluid style={{ maxWidth: '200px', maxHeight: '200px' }} />
                     </Col>
                     <Col md={6}>
                         <h6 className="text-primary">Twitter</h6>
                     </Col>
                     <Col md={6} className="d-flex align-items-start justify-content-start flex-column">
                         <h3 style={{ borderBottom: '2px solid #191970' }}>
-                            {user.first_name} {user.last_name}
+                           Name : {user.first_name} {user.last_name}
                         </h3>
-                        <p className="text-primary">{user.email}</p>
+                        <p className="text-primary">Email : {user.email}</p>
                         <p>Images Created: {post.image}</p>
                     </Col>
                 </Row>
             </Container>
-            <Footer />
         </>
     );
 }
