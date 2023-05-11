336609420d1e9a8a01c7b734973a7afaa0fa79bd
Bryan Caldera <bryanicaldera@gmail.com>
2023-05-10 23:36:40 -0700

    nav changes

---
+++ frontend/website/src/pages/Logout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Logout() {
+
+    //TODO code to log out
+    //TODO redirect to wherever
+
+    return (
+        <div className="d-flex justify-content-center">
+            <h1>Logout</h1>
+        </div>
+    );
+}
