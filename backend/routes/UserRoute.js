d4ac6a2400d86348d8d28e993306e29e69a4a686
nicklin-sjsu <nicklin1219@gmail.com>
2023-05-11 00:04:35 -0700

    Add getcurrent user

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const verify = require("../config/verify");
-const {getByID, editByID} = require("../controllers/UserController");
+const {getByID, editByID, getCurrentUser} = require("../controllers/UserController");
 
 router.get("/getByID", verify, getByID);
+router.get("/getCurrentUser", verify, getCurrentUser);
 router.get("/editByID", verify, editByID);
 
 module.exports = router;
