5532eb1009afdceed4c9f54a28c22595e8f2ea18
Bryan Caldera <bryanicaldera@gmail.com>
2023-05-11 00:15:03 -0700

    login sessions

--- frontend/website/src/pages/Signup.js
+++ frontend/website/src/pages/Signup.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import SignUpCard from "../components/SignUpCard";
+import axios from "axios";
 
 export default function Signup() {
+
+  const history = useHistory();
+
+  useEffect(() => {
+    // some check somehow
+    axios.get("http://localhost:5003/login")
+      .then((res) => {
+        console.log(res);
+        history.push("/");
+      }
+      ).catch((err) => {
+        console.log(err);
+      }
+      )
+  }, [history]);
+
+
   return (
     <div className="d-flex justify-content-center">
-      <SignUpCard />
+      <p>Sign Up</p>
     </div>
   );
 }
