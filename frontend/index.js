c0778189c1836d28ef51747e2f7c4782b971dd6c
Bryan Caldera <bryanicaldera@gmail.com>
2023-05-11 04:03:06 -0700

    deploy frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,15 @@
 // write a hello world expresss app
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3006;
-app.get('/', (req, res) => res.send('Hello World! One more time!'));
+
+app.use(express.static(path.join(__dirname, 'website', 'build')));
+
+app.get('*', (req, res) => {
+  //res.sendFile(path.join(__dirname, '../..', 'front-end/my-app/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'website', 'build', 'index.html'));
+});
+
+//app.get('/', (req, res) => res.send('Hello World! One more time!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
