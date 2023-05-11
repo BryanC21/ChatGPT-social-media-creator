3309f6f25f5dc90133a15c24a2a659e3a07652f9
RamDara07 <61180970+RamDara07@users.noreply.github.com>
2023-05-10 23:43:54 -0700

    Add files via upload

---
+++ frontend/website/src/components/HomeInput.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+function HomeInput(props) {
+  const { textInput, handleTextInputChange, handleMediaInputChange, generateSummary, handleTweetInputChange, postTweet } = props;
+  const [fileType, setFileType] = useState('text'); // Default file type is set to 'text'
+  const [tweetInput, setTweetInput] = useState('');
+
+  const handleGenerateClick = () => {
+    generateSummary(textInput);
+  };
+
+  const handleFileTypeChange = (event) => {
+    setFileType(event.target.value);
+  };
+
+  const handlePostTweet = () => {
+    postTweet(tweetInput);
+    setTweetInput('');
+  };
+
+  return (
+    <div>
+      <div className="card" style={{ width: '35rem' }}>
+        <div className="card-body">
+          <div className="text-center">
+            <h5 className="card-title">Generate Summary</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Text, Audio and Video are supported.</h6>
+          </div>
+
+          <Form>
+            <Form.Group className="mb-3" controlId="file-type-select">
+              <Form.Label>Select File Type:</Form.Label>
+              <Form.Select value={fileType} onChange={handleFileTypeChange}>
+                <option value="text">Text</option>
+                <option value="audioVideo">Audio/Video</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>{fileType === 'text' ? 'Text' : 'Audio/Video'} input:</Form.Label>
+              {fileType === 'text' ? (
+                <Form.Control as="textarea" rows={3} value={textInput} onChange={handleTextInputChange} />
+              ) : (
+                <input type="file" id="media-input" accept="audio/*,video/*" multiple onChange={handleMediaInputChange} />
+              )}
+            </Form.Group>
+
+            <Button onClick={handleGenerateClick}>Generate</Button>
+
+            <Form.Group className="mt-3" controlId="exampleForm.ControlTextarea2">
+              <Form.Label>Generated Tweet:</Form.Label>
+              <Form.Control as="textarea" rows={3} value={tweetInput} onChange={handleTweetInputChange} />
+            </Form.Group>
+
+            <Button variant="primary" onClick={handlePostTweet}>Post Tweet</Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeInput;
+
